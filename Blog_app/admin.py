ffbf8103dc5d70a448b247f248b243dfbd2b80cc
Rajesh Thapa <rajesh025thapa@gmail.com>
2026-03-13 15:14:24 +0545

    good going

--- Blog_app/admin.py
+++ Blog_app/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from .models import Category, Blog
 # Register your models here.
 class CategoryAdmin(admin.ModelAdmin):
-  list_display = ['id', 'category_name', 'created_At', 'updated_At',]
+  list_display = ['id', 'category_name', 'created_At', 'updated_At']
   ordering = ['id']
 
 class BlogAdmin(admin.ModelAdmin):
-  list_display = ['id', 'title', 'category', 'author', 'status','is_Featured']
+  list_display = ['id', 'title', 'category', 'author', 'status','is_Featured','category_id','author_id']
   prepopulated_fields = {'slug': ('title',)}
   ordering = ['id']
 
--- Blog_app/views.py
+++ Blog_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Blog, Category
 
 # Create your views here.
+def post_by_category(request, category_id):
+  category = get_object_or_404(Category, pk=category_id)
+  category_post = Blog.objects.filter(category=category, status="Published")
+  context = {
+    "category": category,
+    "category_post": category_post,
+  }
+  return render(request, 'post_by_category.html', context)
