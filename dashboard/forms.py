2658f318c8eff9bd506f8e76afd700d3ec8718b1
Rajesh Thapa <rajesh025thapa@gmail.com>
2026-03-24 09:31:58 +0545

    category functionality completed

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -2,6 +2,6 @@ from django import forms
 from Blog_app.models import Category
 
 class CategoryForm(forms.ModelForm):
-  class Meta:
-    model = Category
-    fields = ('__all__')
+    class Meta:
+        model = Category
+        fields = '__all__'
